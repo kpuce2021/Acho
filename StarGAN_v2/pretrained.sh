3f4ac0a5910e2dd6ff032d28a695efe46e458770
tae1010 <53727139+tae1010@users.noreply.github.com>
2021-02-02 23:57:23 +0900

    Update pretrained.sh

--- StarGAN_v2/pretrained.sh
+++ StarGAN_v2/pretrained.sh
@@ -1,5 +1,12 @@
-if [$FILE == "2000-pretrained"]; then
+FILE=$1
+
+if [ $FILE == "2000-pretrained" ]; then
     URL=https://drive.google.com/drive/folders/1BDhtEjzXHg-DvqgEHKdM06vPSeBmuMxz?usp=sharing
     mkdir -p ./expr/checkpoints/
     OUT_FILE=./expr/checkpoints/002000_nets_ema.ckpt
     wget -N $URL -O $OUT_FILE
+
+else
+    echo "re"
+    exit 1
+fi
