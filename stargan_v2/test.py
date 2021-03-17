ef7c758798883f1eeb6246236fe9f0eb7e7e1036
js20028 <68904961+js20028@users.noreply.github.com>
2021-03-17 19:27:41 +0900

    Add files via upload

---
+++ stargan_v2/test.py
@@ -0,0 +1,81 @@
+"""
+StarGAN v2
+Copyright (c) 2020-present NAVER Corp.
+
+This work is licensed under the Creative Commons Attribution-NonCommercial
+4.0 International License. To view a copy of this license, visit
+http://creativecommons.org/licenses/by-nc/4.0/ or send a letter to
+Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
+"""
+
+import os
+import argparse
+import shutil
+
+from munch import Munch
+from torch.backends import cudnn
+import torch
+
+from stargan_v2.core.data_loader import get_train_loader
+from stargan_v2.core.data_loader import get_test_loader
+from stargan_v2.core.solver import Solver
+
+
+def str2bool(v):
+    return v.lower() in ('true')
+
+
+def subdirs(dname):
+    return [d for d in os.listdir(dname)
+            if os.path.isdir(os.path.join(dname, d))]
+
+
+def stargan(args):
+    print(args)
+    cudnn.benchmark = True
+    torch.manual_seed(args.seed)
+
+    solver = Solver(args)
+
+    if args.mode == 'train':
+        assert len(subdirs(args.train_img_dir)) == args.num_domains
+        assert len(subdirs(args.val_img_dir)) == args.num_domains
+        loaders = Munch(src=get_train_loader(root=args.train_img_dir,
+                                             which='source',
+                                             img_size=args.img_size,
+                                             batch_size=args.batch_size,
+                                             prob=args.randcrop_prob,
+                                             num_workers=args.num_workers),
+                        ref=get_train_loader(root=args.train_img_dir,
+                                             which='reference',
+                                             img_size=args.img_size,
+                                             batch_size=args.batch_size,
+                                             prob=args.randcrop_prob,
+                                             num_workers=args.num_workers),
+                        val=get_test_loader(root=args.val_img_dir,
+                                            img_size=args.img_size,
+                                            batch_size=args.val_batch_size,
+                                            shuffle=True,
+                                            num_workers=args.num_workers))
+        solver.train(loaders)
+    elif args.mode == 'styling':
+        assert len(subdirs(args.src_dir)) == args.num_domains
+        assert len(subdirs(args.ref_dir)) == args.num_domains
+        loaders = Munch(src=get_test_loader(root=args.src_dir,
+                                            img_size=args.img_size,
+                                            batch_size=args.batch_size,
+                                            shuffle=False,
+                                            num_workers=args.num_workers),
+                        ref=get_test_loader(root=args.ref_dir,
+                                            img_size=args.img_size,
+                                            batch_size=args.batch_size,
+                                            shuffle=False,
+                                            num_workers=args.num_workers))
+        solver.sample(loaders)
+    elif args.mode == 'eval':
+        solver.evaluate()
+    elif args.mode == 'align':
+        from core.wing import align_faces
+        align_faces(args, args.inp_dir, args.out_dir)
+    else:
+        raise NotImplementedError
