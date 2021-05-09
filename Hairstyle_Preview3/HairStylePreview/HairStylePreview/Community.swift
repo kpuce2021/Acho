4fe7551817c7ce38a6d3fd2e49414d98fe8c7854
tae1010 <53727139+tae1010@users.noreply.github.com>
2021-05-09 21:04:50 +0900

    Add files via upload

---
+++ Hairstyle_Preview3/HairStylePreview/HairStylePreview/Community.swift
@@ -0,0 +1,22 @@
+//
+//  Community.swift
+//  HairStylePreview
+//
+//  Created by 김정태 on 2021/04/05.
+//
+//community
+
+import UIKit
+
+class Community: UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        
+        
+        
+        
+        
+    }
+}
---
+++ Hairstyle_Preview3/HairStylePreview/HairStylePreview/Result.swift
@@ -0,0 +1,19 @@
+//
+//  Result.swift
+//  HairStylePreview
+//
+//  Created by 곽재선 on 2021/05/07.
+//
+
+import UIKit
+
+class Result: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+}
