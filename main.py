1405c54c8c3140c46766d1b33ce76c4c8b470ce8
tae1010 <53727139+tae1010@users.noreply.github.com>
2021-05-19 18:45:23 +0900

    Update main.py

--- main.py
+++ main.py
@@ -138,21 +138,21 @@ if __name__ == '__main__':
     parser.add_argument('--latent_dim', type=int, default=16, help='Latent vector dimension')
     parser.add_argument('--hidden_dim', type=int, default=512, help='Hidden dimension of mapping network')
     parser.add_argument('--style_dim', type=int, default=64,help='Style code dimension')
-    parser.add_argument('—w_hpf', type=float, default=1, help='weight for high-pass filtering')
+    parser.add_argument('--w_hpf', type=float, default=1, help='weight for high-pass filtering')
 
-    parser.add_argument('—resume_iter', type=int, default=110000,help='Iterations to resume training/testing')
-    parser.add_argument('—checkpoint_dir', type=str, default='pretrained_network/StarGAN')
-    parser.add_argument('—wing_path', type=str, default='pretrained_network/StarGAN/wing.ckpt')
+    parser.add_argument('--resume_iter', type=int, default=110000,help='Iterations to resume training/testing')
+    parser.add_argument('--checkpoint_dir', type=str, default='pretrained_network/StarGAN')
+    parser.add_argument('--wing_path', type=str, default='pretrained_network/StarGAN/wing.ckpt')
 
-    parser.add_argument('—src_dir', type=str, default='./data/src')
-    parser.add_argument('—result_dir', type=str, default='./results/img')
+    parser.add_argument('--src_dir', type=str, default='./data/src')
+    parser.add_argument('--result_dir', type=str, default='./results/img')
     
     # for styling_ref
-    parser.add_argument('—ref_dir', type=str, default='./data/ref')
+    parser.add_argument('--ref_dir', type=str, default='./data/ref')
 
     # for styling_rand
-    parser.add_argument('—target_domain', type=int, default=0)
-    parser.add_argument('—num_outs_per_domain', type=int, default=3)
+    parser.add_argument('--target_domain', type=int, default=0)
+    parser.add_argument('--num_outs_per_domain', type=int, default=3)
     
     args = parser.parse_args()
     main(args)
